5d2f21e827f610ca7b1d6c8c7394e3bf7e4e9d45
phuocsanh <phuocsanhtps@gmail.com>
2024-08-27 08:55:06 +0700

    update

--- src/app/api/sell/route.ts
+++ src/app/api/sell/route.ts
@@ -5,37 +5,44 @@ import { createSignature } from "@/ultil/createSignature";
 export async function POST(req: NextRequest) {
   try {
     const { symbol, quantity, price, apiSecret, accesskey } = await req.json();
-    const params: Record<string, any> = {
-      symbol: symbol + "USDT",
-      side: "SELL",
-      type: "LIMIT",
-      quantity,
-      price,
-    };
 
-    const parametersArray = Object.keys(params).map((key: any) => ({
-      key,
-      value: params[key],
-      disabled: false, // Hoặc sử dụng giá trị thực tế nếu có
-    }));
+    const requests = Array.from({ length: 9 }).map(async () => {
+      const params: Record<string, any> = {
+        symbol: symbol + "USDT",
+        side: "SELL",
+        type: "LIMIT",
+        quantity,
+        price,
+      };
 
-    const response = await axios.post(
-      "https://api.mexc.com/api/v3/order",
-      null,
-      {
-        params: {
-          ...params,
-          timestamp: Date.now(),
-          signature: createSignature(parametersArray, apiSecret),
-        },
-        headers: {
-          "Content-Type": "application/json",
-          "X-MEXC-APIKEY": accesskey,
-        },
-      }
-    );
+      const parametersArray = Object.keys(params).map((key: any) => ({
+        key,
+        value: params[key],
+        disabled: false, // Hoặc sử dụng giá trị thực tế nếu có
+      }));
 
-    return NextResponse.json(response.data);
+      const response = await axios.post(
+        "https://api.mexc.com/api/v3/order",
+        null,
+        {
+          params: {
+            ...params,
+            timestamp: Date.now(),
+            signature: createSignature(parametersArray, apiSecret),
+          },
+          headers: {
+            "Content-Type": "application/json",
+            "X-MEXC-APIKEY": accesskey,
+          },
+        }
+      );
+
+      return response.data;
+    });
+
+    const results = await Promise.all(requests);
+
+    return NextResponse.json(results);
   } catch (error: any) {
     console.error("Error occurred:", error);
     return NextResponse.json({ error: error.message }, { status: 400 });
